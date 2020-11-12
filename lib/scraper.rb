a399229f8dda96964894ce528ef75787394abfb8
jeremysantana <santanajay21@gmail.com>
2020-11-11 19:15:27 -0500

    commit tree dbdae4b218251e726f3474cf3ddb4c995531f1ce

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+Nokogiri::HTML(html) #method to take the string of HTML returned by open-uri's `open` method and convert it into a NodeSet
+doc = Nokogiri::HTML(html) #save the HTML document in a variable, `doc` that we can then operate on
+
+html
+<div id="my-div">
+  <p class="my-paragraph"></p>
+</div>
